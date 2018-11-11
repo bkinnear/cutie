91aa372236cde6e609cff1dbde637f909a7a4a95
bkinnear <bkinn002@ucr.edu>
2018-11-11 20:44:17 +0000

    main.js update

--- main.js
+++ main.js
@@ -1,15 +1,68 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
-function checkKey(e) {
-    var event = window.event ? window.event : e;
-    console.log(event.keyCode)
+window.addEventListener('keydown',this.check,false);
+
+var player = {
+	x:0,
+	y:0,
+	speed:4
+}
+
+var onUpdate = {
+	moveLeft: false,
+	moveRight: false,
+	moveUp: false,
+	moveDown: false
+}
+
+
+function check(e) {
+	var code = e.keyCode;
+
+	switch (code) {
+	case 37:	//left
+		onUpdate.moveLeft = true;
+		console.log(code);
+	case 38:	//up
+		onUpdate.moveUp = true;
+		console.log(code);
+	case 39:	//right
+		onUpdate.moveRight = true;
+		console.log(code);
+	case 40:	//down
+		onUpdate.moveDown = true;
+		console.log(code);
+	}
+
+}
+
+function drawCircle(x, y, r, c) {
+	ctx.beginPath();
+	ctx.arc(x, y, r, 0, 2*Math.PI);
+	ctx.fillStyle = c;
+	ctx.fill();
 }
 
 function update() {
-	ctx.beginPath();
-	ctx.arc(Math.random()*95, Math.random()*50, Math.random()*40, 0, 2 * Math.PI);
-	ctx.stroke();
+	drawCircle(player.x, player.y, 16, "FF0000");
+
+	if (onUpdate.moveLeft) {
+		onUpdate.moveLeft = false;
+		player.x -= player.speed;
+	}
+	if (onUpdate.moveRight) {
+		onUpdate.moveRight = false;
+		player.x += player.speed;
+	}
+	if (onUpdate.moveUp) {
+		onUpdate.moveUp = false;
+		player.y -= player.speed;
+	}
+	if (onUpdate.moveDown) {
+		onUpdate.moveDown = false;
+		player.y += player.speed;
+	}
 }
 
 setInterval(update, 25);
