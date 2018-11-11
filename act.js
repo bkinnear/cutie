cf6e2ae44e1c600961f0a44863534e52cfa4b6f5
San234 <allen525@naver.com>
2018-11-11 15:04:02 -0800

    Update act.js

--- act.js
+++ act.js
@@ -1,9 +1,13 @@
 var checkShopUI = 0;
 var shopUI = document.getElementById("shopUI");
+var hpBuy = document.getElementById("hpBuy");
+var mpBuy = document.getElementById("mpBuy");
 var shopUI2d = shopUI.getContext("2d");
 
-shopUI2d.font="24px Consolas";
-shopUI2d.fillText("Shop", 230, 40);
+shopUI2d.font="30px Consolas";
+shopUI2d.fillText("Shop", 230, 30);
+shopUI2d.fillText("Red Potion", 30, 90);
+shopUI2d.fillText("Blue Potion", 30, 190);
 
 function shop(){
 	elem = document.getElementById("string");
@@ -11,10 +15,14 @@ function shop(){
 	
 	if(checkShopUI == 0){
 		shopUI.style.display = 'inline';
+		hpBuy.style.display = 'inline';
+		mpBuy.style.display = 'inline';
 		checkShopUI = 1;
 	}
 	else{
 		shopUI.style.display = 'none';
+		hpBuy.style.display = 'none';
+		mpBuy.style.display = 'none';
 		checkShopUI =0;
 	}
 }
