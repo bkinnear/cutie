226dfc2d7dd1acf18fefa79c6f2254e2cc006466
San234 <allen525@naver.com>
2018-11-11 15:52:54 -0800

    Update act.js

--- act.js
+++ act.js
@@ -1,7 +1,7 @@
 var checkShopUI = 0;
 var shopUI = document.getElementById("shopUI");
-var hpBuy = document.getElementById("hpBuy");
-var mpBuy = document.getElementById("mpBuy");
+var shopItem = document.getElementsByClassName("shop");
+console.log(shopItem);
 var shopUI2d = shopUI.getContext("2d");
 
 shopUI2d.font="30px Consolas";
@@ -10,21 +10,30 @@ shopUI2d.fillText("Red Potion", 30, 90);
 shopUI2d.fillText("Blue Potion", 30, 190);
 
 function shop(){
-	elem = document.getElementById("string");
-	elem.style.color = 'red';
+	elem = document.getElementsByClassName("string");
+	elem[0].style.color = 'red';
 	
 	if(checkShopUI == 0){
 		shopUI.style.display = 'inline';
-		hpBuy.style.display = 'inline';
-		mpBuy.style.display = 'inline';
+		for(var i=0; i < shopItem.length; i++){
+			shopItem[i].style.display = 'inline';
+		}
 		checkShopUI = 1;
 	}
 	else{
 		shopUI.style.display = 'none';
-		hpBuy.style.display = 'none';
-		mpBuy.style.display = 'none';
+		for(var i=0; i < shopItem.length; i++){
+			shopItem[i].style.display = 'none';
+		}
 		checkShopUI =0;
 	}
+}
+function shopClose(){
+	shopUI.style.display = 'none';
+	for(var i=0; i < shopItem.length; i++){
+		shopItem[i].style.display = 'none';
+	}
+	checkShopUI = 0;
 }
 function update(){
 	
