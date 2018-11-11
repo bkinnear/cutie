ecd7c93f4b7c0bb7b9e80d56f84fc5335daeadf6
bkinnear <bkinn002@ucr.edu>
2018-11-11 19:47:05 +0000

    js

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
-ctx.beginPath();
-ctx.arc(95, 50, 40, 0, 2 * Math.PI);
-ctx.stroke();
+function checkKey(e) {
+    var event = window.event ? window.event : e;
+    console.log(event.keyCode)
+}
+
+function update() {
+	ctx.beginPath();
+	ctx.arc(Math.random()*95, Math.random()*50, Math.random()*40, 0, 2 * Math.PI);
+	ctx.stroke();
+}
+
+setInterval(update, 25);
