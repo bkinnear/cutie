be394b0d422a00bbf2c9814bd8f6d729fd9d4981
San234 <allen525@naver.com>
2018-11-11 20:19:53 -0800

    Update act.js

--- act.js
+++ act.js
@@ -12,6 +12,8 @@ shopUI2d.fillText("Blue Potion\t$20", 30, 190);
 numCoins2d.font="12px Consolas";
 numCoins2d.fillText("Inventory", 100, 15);
 
+printAmount();
+
 function shop(){
 	elem = document.getElementsByClassName("string");
 	elem[0].style.color = 'red';
@@ -40,22 +42,22 @@ function shopClose(){
 }
 function inventory(inputItem){
 	if(inputItem == 'redPotion'){
-		if(player.redPotion < 1){
+		if(player.redPotions < 1){
 			alert("You are punished.");
 			player.hp -= 5;
 		}
 		else{
 			player.hp += 20;
-			player.redPotion -= 1;
+			player.redPotions -= 1;
 		}
 	}
 	else if(inputItem == 'bluePotioin'){
-		if(player.bluePotion < 1){
+		if(player.bluePotions < 1){
 			alert("You are punished.");
 		}
 		else{
 			player.mp += 40;
-			player.bluePotion -= 1;
+			player.bluePotions -= 1;
 		}
 	}
 	else if(inputItem == 'sword'){
@@ -64,31 +66,51 @@ function inventory(inputItem){
 	else if(inputItem == 'minigun'){
 		player.equipped = "ranged";
 	}
+	numCoins2d.fillStyle = "#808080";
+	numCoins2d.fillRect(0,0,numCoins.width, numCoins.height);
+	printAmount();
 }
 function buy(potionKind){
 	if(player.coins >= 20){
-		if(potionKind = 'hp'){
+		if(potionKind == 'hp'){
 			//add redpotion by 1
-			player.redPotion += 1;
+			player.redPotions += 1;
 			player.coins -= 20;
+			
 		}
 		else{
 			//add bluepotion by 1
-			player.bluePotion += 1;
+			player.bluePotions += 1;
 			player.coins -= 20;
 		}
 	}
 	else{
 		alert("You don't have enough money");
 	}
+	numCoins2d.fillStyle = "#808080";
+	numCoins2d.fillRect(0,0,numCoins.width, numCoins.height);
+	printAmount();
 }
 function printAmount(){
+	numCoins2d.fillStyle = "#000000";
 	numCoins2d.fillText(": "+player.coins, 190, 120);
 	numCoins2d.fillText(": "+player.redPotions, 190, 132);
 	numCoins2d.fillText(": "+player.bluePotions, 190, 144);
-
+	
 }
 function quit(){
 	player.hp -= 1;
 }
-
+function upgrade(upgradeKind){
+	if(upgradeKind == 'armour'){
+		player.armour += 3;
+		player.coins -= 25;
+	}
+	else{
+		player.damage += 3;
+		player.coins -= 25;
+	}
+	numCoins2d.fillStyle = "#808080";
+	numCoins2d.fillRect(0,0,numCoins.width, numCoins.height);
+	printAmount();
+}
