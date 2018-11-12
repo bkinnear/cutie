cc1a70d11b69d8a929094ef4a9e20ebfddbbc910
bkinnear <bkinn002@ucr.edu>
2018-11-12 01:45:07 +0000

    ?

--- player.js
+++ player.js
@@ -1,5 +1,8 @@
 var player = {
 	hp:100,
+	damage:1,
+	armour:1,
+	sinepower:1,
         x:300,
         y:240,
         direction:0,
--- status.js
+++ status.js
@@ -2,10 +2,30 @@ var statCanvas = document.getElementById("status");
 var statCtx = canvas.getContext("2d");
 
 function statusUpdate() {
+	// player hp bar
 	statCtx.fillStyle = "#000000";
 	statCtx.fillRect(0, 0, statCanvas.width, 32);
-	statCtx.fillStyle = "#00FF00"
-	ststCtx.fillRect(0, 0, statCanvas.width*(hp/100), 32)
+	statCtx.fillStyle = "#00FF00";
+	statCtx.fillRect(0, 0, statCanvas.width*(player.hp/100), 32);
+
+	// player armour
+	statCtx.fillStyle = "000000";
+	statCtx.fillRect(0, 64, statCanvas.width, 32);
+	statCtx.fillStyle = "888888";
+	statCtx.fillRect(0, 64, statCanvas.width*(player.armour/100), 32);
+
+	// player damage
+	statCtx.fillStyle = "000000";
+	statCtx.fillRect(0, 96, statCanvas.width, 32);
+	statCtx.fillStyle = "888888";
+	statCtx.fillRect(0, 96, statCanvas.width*(player.damage/100), 32);
+
+	// sinusoidal power
+	statCtx.fillStyle = "000000";
+	statCtx.fillRect(0, 128, statCanvas.width, 32);
+	statCtx.fillStyle = "888888";
+	statCtx.fillRect(0, 128, statCanvas.width*(player.sinepower/100), 32);
+
 }
 
 setInterval(statusUpdate, 100);
